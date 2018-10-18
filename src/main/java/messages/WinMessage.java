2c00cc6adf7b93514b24bf7d0ed3fb7bd7a61ec3
Ian Rodriguez <irodriguez0456@gmail.com>
2018-10-17 21:15:25 -0600

    Async sucks

---
+++ src/main/java/messages/WinMessage.java
@@ -0,0 +1,13 @@
+package messages;
+
+public class WinMessage {
+    private String wiinner;
+
+    public String getWiinner() {
+        return wiinner;
+    }
+
+    public void setWiinner(String wiinner) {
+        this.wiinner = wiinner;
+    }
+}
